8efe00bf1e770ca48553f83bae682c0420a308a7
Soroush Zare <ihaveint@gmail.com>
2023-03-21 18:05:09 -0700

    Test lookup_node for a randomly generated network

--- lib/node.ex
+++ lib/node.ex
@@ -1,6 +1,5 @@
 defmodule Knode do
   import Bitwise
-  import Math
 
   @k 20
   @a 10
@@ -14,7 +13,7 @@ defmodule Knode do
   }
 
   defstruct(
-    k_buckets: Enum.map(0..160, fn bucket_id -> [] end),
+    k_buckets: Enum.map(0..160, fn _bucket_id -> [] end),
     id: nil,
     data: %{}
   )
@@ -35,30 +34,31 @@ defmodule Knode do
   def send_to_front(buckets, bucket_id, node_id) do
     updated_bucket =
       Enum.at(buckets, bucket_id)
-      |> Enum.slide( node_id |> Enum.find_index(Enum.at(buckets, bucket_id)), -1)
+      |> Enum.slide(Enum.find_index(Enum.at(buckets, bucket_id), fn x -> x == node_id end), -1)
 
-    updated_buckets = 
+    _updated_buckets = 
       buckets
       |> Enum.with_index
       |> Enum.map(fn
-        {bucket, ^bucket_id} -> updated_bucket
+        {_bucket, ^bucket_id} -> updated_bucket
         {bucket, _} -> bucket
       end)
   end
 
   @spec update_buckets(__MODULE__, node_id) :: __MODULE__
-  def update_buckets(state = %{id: id, k_buckets: buckets}, sender = {sender_pid, sender_id}) do
+  def update_buckets(state = %{id: id, k_buckets: buckets}, sender = {_sender_pid, sender_id}) do
     distance = bxor(sender_id, id) 
     bucket_id = floor(Math.log2(distance))
     updated_buckets = if Enum.member?(Enum.at(buckets, bucket_id), sender) do
-      updated_buckets = send_to_front(buckets, bucket_id, sender)
+      _updated_buckets = send_to_front(buckets, bucket_id, sender)
     else
       # if the bucket is full, ping the least recently seen one first. If it answers, discard the sender. 
       # Else, evict it and insert the new sender at the tail
         if Enum.count(Enum.at(buckets, bucket_id)) == @k do
-          [least_recently_seen | t] = Enum.at(buckets, bucket_id)
-          updated_buckets = 
-            case ping(least_recently_seen) do
+          [least_recently_seen | _t] = Enum.at(buckets, bucket_id)
+          _updated_buckets = 
+            #case ping(least_recently_seen, id) do
+            case :alive do
               :alive ->
                 send_to_front(buckets, bucket_id, least_recently_seen)
               :presume_dead ->
@@ -78,34 +78,39 @@ defmodule Knode do
         else
           updated_bucket = Enum.at(buckets, bucket_id) ++ [sender]
 
-          updated_buckets =
+          _updated_buckets =
           buckets
           |> Enum.with_index
           |> Enum.map(fn
-            {bucket, ^bucket_id} -> updated_bucket
+            {_bucket, ^bucket_id} -> updated_bucket
             {bucket, _} -> bucket
           end)
         end
     end
 
-    #IO.inspect("updated buckets for ", label: id)
-    #IO.inspect(updated_buckets)
-    
     %{state | k_buckets: updated_buckets}
   end
 
   @spec loop(__MODULE__) :: any
-  def loop(state = %{id: id}) do
+  def loop(state = %{id: id, k_buckets: buckets}) do
     receive do
+      {:debug, :k_buckets, from} ->
+        send(from, buckets)
+        loop(state)
+      {:join, from, _contact_node = {contact_node_pid, contact_node_id}} ->
+        new_state = update_buckets(state, {contact_node_pid, contact_node_id})
+        lookup(self(), new_state, id)
+        send(from, :joined)
+        loop(new_state)
       {:request, {:store, from, {key, value}}} ->
-        {k_closests, new_state} = lookup(state, key)
-        Enum.map(k_closests, fn {node_pid, node_id} ->
+        k_closests = lookup(self(), state, key)
+        Enum.map(k_closests, fn {node_pid, _node_id} ->
           send(node_pid, {:request, {:rpc_store, {self(), id}, key, value}})
         end)
 
         send(from, :ok)
         
-        loop(new_state)
+        loop(state)
       {:request, {:rpc_store, {from, node_id}, key, value}} -> 
         if node_id != id do
           loop(state |> store(key, value) |> update_buckets({from, node_id}))
@@ -118,28 +123,82 @@ defmodule Knode do
       {:request, {:rpc_ping, {from, node_id}}} -> 
         send(from, :alive)
         loop(state |> update_buckets({from, node_id}))
-      {:request, {:rpc_find_node, {from, node_id}, target_id}} ->
-        {k_closests, new_state} = lookup(state, target_id)
-        send(from, k_closests)
-        loop(new_state |> update_buckets({from, node_id}))
-      {:find_node, from, target_id} ->
-        {k_closests, new_state} = lookup(state, target_id)
-        send(from, k_closests)
+      {:request, {:rpc_find_node, from, initiator = {_initiator_pid, initiator_id}, target_id}} ->
+        spawn(fn ->
+          k_closests = 
+            Enum.map(buckets, fn bucket ->
+              bucket |> Enum.map(fn node = {_node_pid, node_id} ->
+                {bxor(node_id, target_id), node}
+              end)
+            end)
+            |> List.flatten
+            |> Enum.sort()
+            |> Enum.uniq
+            |> Enum.take(@k)
+            |> Enum.map(fn {_distance, node} -> node end)
+
+
+          send(from, k_closests)
+        end)
+
+        if initiator_id != id do
+          loop(state |> update_buckets(initiator))
+        else
+          loop(state)
+        end
+      {:lookup_node, from, target_id}  ->
+        send(from, lookup_node(self(), state, target_id))
         loop(state)
-      {:request, {:rpc_find_value, {from, node_id}, key}} ->
-        {value, new_state} = find_value(key, state)
+      {:find_node, from, target_id} ->
+        spawn(fn ->
+          k_closests = 
+            Enum.map(buckets, fn bucket ->
+              bucket |> Enum.map(fn node = {_node_pid, node_id} ->
+                {bxor(node_id, target_id), node}
+              end)
+            end)
+            |> List.flatten
+            |> Enum.sort()
+            |> Enum.uniq
+            |> Enum.take(@k)
+            |> Enum.map(fn {_distance, node} -> node end)
+
+
+          send(from, k_closests)
+        end)
+        loop(state)
+      {:find_value, from, target_id} ->
+        pid = self()
+        spawn(fn ->
+          value = find_value(pid, target_id, state)
+          send(from, value)
+        end)
+        loop(state)
+      {:request, {:rpc_find_value, from, initiator = {_initiator_pid, _initiator_id}, ^id}} ->
+        data = Map.get(state, :data)
+        value = Map.get(data, id)
         send(from, value)
-        loop(new_state |> update_buckets({from, node_id}))
+        loop(state |> update_buckets(initiator))
+      {:request, {:rpc_find_value, from, initiator = {_initiator_pid, _initiator_id}, key}} ->
+        pid = self()
+        spawn(fn ->
+          value = find_value(pid, key, state)
+          send(from, value)
+        end)
+        loop(state |> update_buckets(initiator))
       {:request, {:find_value, from, key}} ->
-        {value, new_state} = find_value(key, state)
-        send(from, value)
+        pid = self()
+        spawn(fn ->
+          value = find_value(pid, key, state)
+          send(from, value)
+        end)
         loop(state)
       {:reply, {from, node_id}, _} -> loop(state |> update_buckets({from, node_id})) 
     end
   end
 
-  def ping(node) do
-    pid = spawn(fn -> ping_and_wait(node, self()) end)
+  def ping(node, id) do
+    _pid = spawn(fn -> ping_and_wait(node, {self(), id}) end)
     receive do
       :alive -> :alive
       :preesume_dead -> :presume_dead
@@ -147,13 +206,13 @@ defmodule Knode do
   end
 
   def ping_and_wait(node, parent) do
-    send(node, :ping)
+    send(node, {:request, {:rpc_ping, parent}})
 
     receive do
       :alive ->
         send(parent, :ok)
     after
-      1_000 ->
+      100 ->
         send(parent, :presume_dead)
     end
   end
@@ -162,37 +221,39 @@ defmodule Knode do
     %{state | data: Map.put(data, key, value)}
   end
 
-  def find_node(state, node_id) do
-    lookup(state, node_id, :rpc_find_value)
+  def lookup_node(initiator_pid, state, node_id) do
+    lookup(initiator_pid, state, node_id, :rpc_find_node)
   end
 
-  def find_value(key, state) do
-    lookup(state, key, :rpc_find_value)
+  def find_value(pid, key, state) do
+    lookup(pid, state, key, :rpc_find_value)
   end
 
-  defp recursive_lookup(learned_nodes, target_id, queried \\ MapSet.new(), initiator_state = %{id: initiator_id}, rpc_type) do
+  defp recursive_lookup(initiator_pid, learned_nodes, target_id, queried, initiator_state = %{id: initiator_id}, rpc_type) do
     new_learned_nodes =
     learned_nodes
-    |> Task.async_stream(fn {node_pid, node_id} ->
-      send(node_pid, {rpc_type, {self(), initiator_id}, target_id})
+    |> Task.async_stream(fn node = {node_pid, _node_id} ->
+      send(node_pid, {:request, {rpc_type, self(), {initiator_pid, initiator_id}, target_id}})
       receive do
          response when is_list(response) ->
+           send(initiator_pid, {:reply, node, response})
           response |> Enum.map(fn {node_pid, node_id} -> {bxor(node_id, target_id), {node_pid, node_id}} end)
          value ->
+           send(initiator_pid, {:reply, node, value})
            [{:terminate, value}]
       after
-        500 ->
+        100 ->
           :remove
       end
     end)
     |> Enum.map(fn {:ok, value} -> value end)
     |> Enum.reject(fn x -> x == :remove end)
     |> List.flatten()
-    |> Enum.reject(fn {node_pid, node_id} -> Enum.member?(learned_nodes, node_id) end)
+    |> Enum.reject(fn {_node_pid, node_id} -> Enum.member?(learned_nodes, node_id) end)
     
     value? = Enum.find(new_learned_nodes, fn x ->
       case x do
-        {:terminate, value} -> true
+        {:terminate, _value} -> true
         _ -> false
       end
     end)
@@ -201,34 +262,37 @@ defmodule Knode do
       {:terminate, value} = value?
       value
     else
-      k_closests = new_learned_nodes
-      |> Enum.take(2 * @k)
-      |> Enum.sort(&(&1 |> elem(1) <= &2 |> elem(1)))
-      |> Stream.map(fn {distance, node} -> node end)
-      |> Enum.take(@k)
+
+      new_learned_nodes =
+        new_learned_nodes
+        |> Enum.map(fn {_distance, node} -> node end)
+
+      k_closests = 
+        new_learned_nodes
+        |> Enum.sort()
+        |> Enum.uniq
+        |> Enum.take(@k)
 
 
       a_closests = k_closests
       |> Enum.reject(fn {_node_pid, node_id} -> MapSet.member?(queried, node_id) end)
+      |> Enum.uniq
       |> Enum.take(@a)
-
 
       if length(a_closests) == 0 or bxor(hd(a_closests) |> elem(1), target_id) >= bxor(hd(learned_nodes) |> elem(1), target_id) do
         xx = 
-        new_learned_nodes ++ learned_nodes
-        |> Enum.reject(fn {_node_pid, node_id} -> MapSet.member?(queried, node_id) end)
-        |> Enum.map(fn {node_pid, node_id} -> {bxor(node_id, target_id), {node_pid, node_id}} end)
-        |> Enum.sort(&(&1 |> elem(1) <= &2 |> elem(1)))
-        |> Enum.take(@k)
-        |> Task.async_stream(fn {_distance, {node_pid, node_id}} ->
-          send(node_pid, {rpc_type, {self(), initiator_id}, target_id})
+        k_closests
+        |> Task.async_stream(fn node = {node_pid, _node_id} ->
+          send(node_pid, {:request, {rpc_type, self(), {initiator_pid, initiator_id}, target_id}})
           receive do
             response when is_list(response) -> 
+              send(initiator_pid, {:reply, node, response})
               response |> Enum.map(fn {node_pid, node_id} -> {bxor(node_id, target_id), {node_pid, node_id}} end)
             value ->
+              send(initiator_pid, {:reply, node, value})
               {:terminate, value}
           after
-            500 ->
+            100 ->
               :remove
           end
         end)
@@ -239,7 +303,7 @@ defmodule Knode do
         
           value? = Enum.find(xx, fn x ->
             case x do
-            {:terminate, value} -> true
+            {:terminate, _value} -> true
             _ -> false
             end
           end)
@@ -249,38 +313,45 @@ defmodule Knode do
             value
           else
             xx
+            ++ Enum.map(learned_nodes, fn {node_pid, node_id} ->
+              {bxor(node_id, target_id), {node_pid, node_id}}
+            end)
             |> Enum.sort(&(&1 |> elem(1) <= &2 |> elem(1)))
-            |> Stream.map(fn {distance, node} -> node end)
+            |> Stream.map(fn {_distance, node} -> node end)
+            |> Enum.uniq
             |> Enum.take(@k)
           end
       else
-        recursive_lookup(new_learned_nodes ++ learned_nodes, target_id, Enum.reduce(learned_nodes, queried, fn x, acc ->
+        recursive_lookup(initiator_pid, a_closests, target_id, Enum.reduce(a_closests, queried, fn x, acc ->
           MapSet.put(acc, x)
-        end), initiator_id, rpc_type)
+        end), initiator_state, rpc_type)
       end
     end
   end
 
-  def lookup(initiator_state = %{k_buckets: buckets, id: id, data: data}, target_id, rpc_type \\ :rpc_find_node) do
+  def lookup(initiator_pid, initiator_state = %{k_buckets: buckets, id: id, data: data}, target_id, rpc_type \\ :rpc_find_node) do
     alpha_closests = buckets
     |> List.flatten
     |> Stream.map(fn {node_pid, node_id} -> {bxor(node_id, target_id), {node_pid, node_id}} end )
-    |> Enum.take(2 * @a)
     |> Enum.sort()
+    |> Enum.uniq
     |> Enum.take(@a)
     |> Enum.map(fn {_dist, node} -> node end)
 
+
     learned_nodes =
     alpha_closests
-    |> Task.async_stream(fn {node_pid, _node_id} ->
-        send(node_pid, {rpc_type, {self(), id}, target_id})
+    |> Task.async_stream(fn node = {node_pid, _node_id} ->
+      send(node_pid, {:request, {rpc_type, self(), {initiator_pid, id}, target_id}})
        receive do
          response when is_list(response) ->
-           response |> Enum.map(fn {node_pid, node_id} -> {bxor(node_id, target_id), {node_pid, node_id}} end)
+           send(initiator_pid, {:reply, node, response})
+           response |> Enum.map(fn node = {_node_pid, node_id} -> {bxor(node_id, target_id), node} end)
          value ->
+           send(initiator_pid, {:reply, node, value})
            [{:terminate, value}]
        after
-         500 ->
+         100 ->
            :remove
        end
     end)
@@ -288,30 +359,35 @@ defmodule Knode do
     |> Enum.reject(fn x -> x == :remove end)
     |> List.flatten()
 
-    value? = Enum.find(alpha_closests, fn x ->
+    value? = Enum.find(learned_nodes, fn x ->
       case x do
-        {:terminate, value} -> true
+        {:terminate, _value} -> true
         _ -> false
       end
     end)
 
     if value? do
       {:terminate, value} = value?
-      {value, initiator_state}
+      value
     else
+      learned_nodes =
+        learned_nodes
+        |> Enum.map(fn {_distance, node} -> node end)
+
       if length(learned_nodes) > 0 do
-        recursive_lookup(learned_nodes, target_id, MapSet.new([id | alpha_closests]), initiator_state, rpc_type)
+        recursive_lookup(initiator_pid, learned_nodes, target_id, MapSet.new([id | alpha_closests]), initiator_state, rpc_type)
       else
         case rpc_type do
-          :rpc_find_node -> {[{self(), id}], initiator_state} # NOTE: should I do this?
-          :rpc_find_value -> {Map.get(data, target_id), initiator_state}
+          :rpc_find_node -> 
+            [{initiator_pid, id}] # NOTE: should I do this?
+          :rpc_find_value -> Map.get(data, target_id)
         end
       end
     end
 
   end
 
-  def refresh(state, bucket_id) do
+  def refresh(state, _bucket_id) do
     nil # TODO
     state
   end
@@ -321,12 +397,5 @@ defmodule Knode do
     receive do
       state -> state
     end
-  end
-
-  def join(state = %{id: id}, {contact_node_pid, contact_node_id}) do
-    new_state = update_buckets(state, {contact_node_pid, contact_node_id})
-    {_, new_state} = lookup(new_state, id)
-    # TODO: refresh all k-buckets further away than its closest neighbor
-    new_state # TODO: return the final state
   end
 end
--- test/kademlia_test.exs
+++ test/kademlia_test.exs
@@ -2,7 +2,7 @@ defmodule FindValueTest do
   use ExUnit.Case
   doctest Knode
 
-  use Bitwise
+  import Bitwise
   
   test "Storing a value returns :ok" do
     node1 = Knode.new_node(1)
@@ -32,23 +32,83 @@ defmodule FindValueTest do
     end
   end
 
+  #@tag mustexec: true
   test "Finding a node after connecting it to the network works" do
     node1 = Knode.new_node(1)
     node2 = Knode.new_node(2)
+    
+    send(node2, {:join, self(), {node1, 1}})
 
-    node2_state = Knode.get_state(node2)
+    receive do
+      :joined -> nil
+    end
 
-    Knode.join(node2_state, {node1, 1})
+    send(node1, {:debug, :k_buckets, self()})
 
-
-    IO.inspect(node1, label: "node1:")
-    IO.inspect(node2, label: "node2:")
+    receive do
+      buckets -> buckets
+    end
 
     send(node1, {:find_node, self(), 2})
+    k_neighbors =
     receive do
-      x -> IO.inspect(x, label: "k_neighbours:")
+      x -> x
     end
-    assert true
+
+    assert Enum.member?(k_neighbors, {node2, 2})
+  end
+
+  @tag mustexec: true
+  test "Finding a node in a network of hundreds of nodes works" do
+    nodes = Enum.map(1..10, fn id ->
+      {Knode.new_node(id), id}
+    end)
+
+    [first_node | rest] = nodes
+
+    Enum.reduce(
+      rest,
+      [first_node], fn node = {node_pid, _node_id}, network_nodes ->
+        random_connection = {_random_node_pid, _random_node_id} = Enum.random(network_nodes)
+        send(node_pid, {:join, self(), random_connection})
+        receive do
+          :joined -> 
+            nil
+        end
+        [node | network_nodes]
+      end)
+
+    Enum.map(nodes, fn {node_pid, _node_id} ->
+      send(node_pid, {:debug, :k_buckets, self()})
+      
+      receive do
+        buckets -> 
+          buckets 
+      end
+    end)
+
+    discovery_enum =
+      Enum.map(1..100, fn _ ->
+      {random_node_pid, _random_node_id} = Enum.random(nodes)
+      {random_node2_pid, random_node2_id} = Enum.random(nodes)
+
+
+      if random_node_pid == random_node2_pid do
+        true
+      else
+        send(random_node_pid, {:lookup_node, self(), random_node2_id})
+        k_neighbors =
+          receive do
+            x -> x
+          end
+        Enum.member?(k_neighbors, {random_node2_pid, random_node2_id})
+      end
+
+    end)
+
+    assert Enum.member?(discovery_enum, false) == false
+
+    #assert true
   end
 
   test "Finding a value after saving it on another node" do
